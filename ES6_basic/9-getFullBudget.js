be703936b07ca8dfeb9c2961ef6912a3e66e8d75
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2025-03-30 21:25:08 -0300

    arrow and propertirs

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -3,7 +3,7 @@ import getBudgetObject from './7-getBudgetObject';
 export default function getFullBudgetObject(income, gdp, capita) {
   return {
     ...getBudgetObject(income, gdp, capita),
-    getIncomeInDollars: () => `$${income}`,
-    getIncomeInEuros: () => `${income} euros`,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
   };
 }
