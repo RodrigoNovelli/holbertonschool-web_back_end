7179781f8a6b127c7896d90db76c77c7039aebdc
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-04-26 23:02:16 -0300

    a fucntion that returns a string representation of a float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+This module has a funciton that takes
+an int and returns its str representation
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    THis function transforms an int into a str
+    '''
+    return str(n)
