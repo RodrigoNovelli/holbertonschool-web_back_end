138a1b108a2c8a0a30256a6ba25ba2aa71923f02
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-04-27 01:05:26 -0300

    an async routine called wait_n that takes in 2 int arguments (in this order): n and max_delay. And spawns wait_random n times with the specified max_delay. wait_n return the list of all the delays

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,10 +8,11 @@ wait_n return the list of all the delays
 
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List:
     '''
     This function makes an async routine called wait_n
     that takes in 2 int arguments (in this order): n and max_delay.
