2499314302d849910cf70319d6b6611876d25703
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-04-27 01:09:50 -0300

    Adding list type

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -12,7 +12,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''
     This function makes an async routine called wait_n
     that takes in 2 int arguments (in this order): n and max_delay.
