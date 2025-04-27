a66dd63cd952345f607085079444ac1af0943b14
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-04-26 22:53:17 -0300

    Making a function that adds two str

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+This module has a function that concat
+two strings
+'''
+
+
+def concat(a: str, b: str) -> str:
+    '''
+    This function is for concat two str
+    '''
+    return (a + b)
