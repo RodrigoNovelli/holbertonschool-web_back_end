040d3608bdc920d85679f9710844005cfc597e9f
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-05-04 21:04:17 -0300

    Adding ex 1 and 2

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''
+This module is for a coroutine that will loop 10 times,
+each time asynchronously wait 1 second,
+then yield a random number between 0 and 10.
+Using the random module. 
+'''
+
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    '''
+      The coroutine will loop 10 times, each time asynchronously
+    wait 1 second, then yield a random number between 0 and 10.
+    '''
+
+    for _ in range(10):
+        await asyncio.sellep(1)
+        yield random.uniform(0, 10)
