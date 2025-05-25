48eb02f2478edc4b39f33ea52d98a7c260b9f8ec
RodrigoNovelli <RodrigoNovelli21@hotmail.com>
2025-05-25 13:40:29 -0300

    finally?

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-// Listen for user input
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim(); // Get and trim the input
-  // Write the output as expected by the test case
-  process.stdout.write(`Your name is: ${input}\r`);
-  // Gracefully close the input stream
-  process.stdin.end();
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
-// Event handler for process exit
-process.on('exit', () => {
-  console.log('This important software is now closing\n');
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
