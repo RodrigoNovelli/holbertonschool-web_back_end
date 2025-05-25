aac1c6ec67c7991331947be0272ad7e108a1fd0a
RodrigoNovelli <RodrigoNovelli21@hotmail.com>
2025-05-25 13:14:59 -0300

    fixing

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,10 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-// Listen for user input on stdin
 process.stdin.on("data", (data) => {
   const input = data.toString().trim();
   process.stdout.write(`Your name is: ${input}\r`);
-  
-  // Gracefully close the input stream after handling input
-  process.stdin.end();
 });
 
-// Event handler for process exit
 process.on("exit", () => {
   console.log("This important software is now closing\n");
 });
