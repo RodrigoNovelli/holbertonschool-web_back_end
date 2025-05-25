bc73e4951de5a774de427414fcd8e1e9b9973908
RodrigoNovelli <RodrigoNovelli21@hotmail.com>
2025-05-25 14:18:02 -0300

    ex 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const app = http.createServer((req, rest) => {
+  rest.writeHead(200, { 'Content-Type': 'text/plain' });
+  rest.end('Hello Holberton School!');
+});
+
+app.listen(1245, () => {
+  console.log('Server is running on http://localhost:1245');
+});
+
+module.exports = app;
