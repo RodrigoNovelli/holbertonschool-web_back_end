508282d46f4ff60d55ee0e392ef1b28df2171f02
RodrigoNovelli <RodrigoNovelli21@hotmail.com>
2025-05-25 13:36:40 -0300

    /r

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const input = data.toString().trim(); // Get and trim the input
   // Write the output as expected by the test case
-  process.stdout.write(`Your name is: ${input}\n`);
+  process.stdout.write(`Your name is: ${input}\r`);
   // Gracefully close the input stream
   process.stdin.end();
 });
