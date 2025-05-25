0e756f91aebe014eaddb57a865d60bbe18a06bc1
RodrigoNovelli <RodrigoNovelli21@hotmail.com>
2025-05-25 13:11:13 -0300

    r isntead of n

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ process.stdout.write("Welcome to Holberton School, what is your name?\n");
 // Listen for user input on stdin
 process.stdin.on("data", (data) => {
   const input = data.toString().trim();
-  process.stdout.write(`Your name is: ${input}\n`);
+  process.stdout.write(`Your name is: ${input}\r`);
   
   // Gracefully close the input stream after handling input
   process.stdin.end();
