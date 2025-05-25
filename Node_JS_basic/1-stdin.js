a6dea136ace7e6b134751958475a23e2004ff30b
RodrigoNovelli <RodrigoNovelli21@hotmail.com>
2025-05-25 13:08:39 -0300

    solving problems with stdin event name

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,15 @@
-process.stdout.write("Welcome to Holberton School, what is your name?");
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.stdin.on("name", (name) => {
-  const input = name.toString().trim();
-  process.stdout.write('Your name is: ${input}\r')
+// Listen for user input on stdin
+process.stdin.on("data", (data) => {
+  const input = data.toString().trim();
+  process.stdout.write(`Your name is: ${input}\n`);
+  
+  // Gracefully close the input stream after handling input
+  process.stdin.end();
 });
 
+// Event handler for process exit
 process.on("exit", () => {
   console.log("This important software is now closing\n");
-});
-
-process.stdin.on("end", () => {
-  process.exit
 });
